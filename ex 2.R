eaf010724441247ec56134f3b531fa175200fbba
sakthiabisheik-web <sakthiabisheik@gmail.com>
2026-03-28 20:46:25 +0530

    Update ex 2.R

--- ex 2.R
+++ ex 2.R
@@ -1,14 +1,26 @@
+u<-c(50,70,40,60,25,50)
+p<-c(30,20,25,45,30,90)
+cat('Sum of units sold:',sum(u),"\n")
+cat('Mean price:',mean(p),"\n",'Mean units sold:',mean(u))
 
-product <- c("Apples", "Bananas", "Oranges", "Grapes", "Musk melon", "Water melon")
-units_sold <- c(50, 70, 40, 60, 25, 50)
-price <- c(30, 20, 25, 45, 30, 90)
+l<-list('john','male','88,92,78,85,85,75,858,56,42')
+cat('gender of student:',l[[2]],"\n")
+cat("students marks:",l[[3]])
 
+m<-matrix(c(1500,1000,1800,2000,1200,2500,3000,1500,2800,900,1600,2100),nrow=3)
+rownames(m)<-c("Jan","Feb","March");colnames(m)<-c("Product A","Product B","Product C"
+                                                   ,"Product D")
+m
+cat("Product B sales in Feb:",m["Feb","Product B"],"\n")
+cat("Product D sales in March:",m["March","Product D"])
 
-total_units <- sum(units_sold)
-mean_price <- mean(price)
-mean_units <- mean(units_sold)
-
-
-cat("Total Units Sold =", total_units, "\n")
-cat("Mean Price =", mean_price, "\n")
-cat("Mean Units Sold =", mean_units, "\n")
+id<-c(101,102,103,104,105,106,107,108,109,110)
+name<-c('notebook','pen','T-shirt','coffee mug','backpack','notebook','pen','T-shirt','coffee mug','backpack')
+category<-c('stationary','stationary','apparel','kitchenware','bags','stationary','stationary','apparel','kitchenware','bags')
+price<-c(50,10,250,150,800,50,10,250,150,800)
+sold<-c(30,100,25,15,8,30,100,25,15,8)
+p<-data.frame(id,name,category,price,sold)
+p$category<-as.factor(p$category)
+cat('Various Category:','\n',levels(p$category),'\n')
+cat('Count of each category:','\n')
+print(table(p$category))
