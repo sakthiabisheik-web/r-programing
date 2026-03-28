727012371bb47c3a6a5072252ebac7939bb57ed1
sakthiabisheik-web <sakthiabisheik@gmail.com>
2026-03-28 20:45:44 +0530

    Update ex 1.R

--- ex 1.R
+++ ex 1.R
@@ -1,66 +1,28 @@
+10+20
+150/2
+58*65
+t=80-25
+print(t)
+2^3
 
+x<-10;y<-5;z<-x+y
+print(z)
 
-cat("===== ARITHMETIC OPERATIONS =====\n")
+class(x)
+class("hello")
+class(TRUE)
+class(38)
 
-a1 <- 10 + 20
-cat("10 + 20 =", a1, "\n")
+sqrt(36)
+log(100)
+exp(2)
+round(3.14149,2)
 
-a2 <- 150 / 2
-cat("150 / 2 =", a2, "\n")
+seq(1,20,by=5)
+rep("MBA",3)
 
-a3 <- 58 * 65
-cat("58 * 65 =", a3, "\n")
-
-t <- 80 - 25
-cat("80 - 25 =", t, "\n")
-
-a4 <- 2^3
-cat("2^3 =", a4, "\n\n")
-
-
-cat("===== VARIABLE ASSIGNMENT =====\n")
-
-x <- 10
-y <- 5
-z <- x + y
-cat("Value of z (x + y) =", z, "\n\n")
-
-
-cat("===== DATA TYPES =====\n")
-
-cat("Class of x:", class(x), "\n")
-cat("Class of 'Hello':", class("Hello"), "\n")
-cat("Class of TRUE:", class(TRUE), "\n\n")
-
-
-cat("===== MATHEMATICAL FUNCTIONS =====\n")
-
-cat("Square root of 49 =", sqrt(49), "\n")
-cat("Log of 100 (natural log) =", log(100), "\n")
-cat("Exponential of 2 =", exp(2), "\n")
-cat("Rounded value of 3.14159 =", round(3.14159, 2), "\n\n")
-
-
-cat("===== SEQUENCE AND REPETITION =====\n")
-
-cat("Sequence from 1 to 20 by 5:\n")
-print(seq(1, 20, by = 5))
-
-cat("Repeat 'MBA' 3 times:\n")
-print(rep("MBA", 3))
-cat("\n")
-
-
-cat("===== SUMMARY STATISTICS =====\n")
-
-marks <- c(80, 85, 92, 74, 68)
-
-cat("Marks:", marks, "\n")
-cat("Mean:", mean(marks), "\n")
-cat("Median:", median(marks), "\n")
-cat("Standard Deviation:", sd(marks), "\n")
-cat("Summary:\n")
-print(summary(marks))
-
-cat("\n===== PROGRAM COMPLETED SUCCESSFULLY =====\n")
-
+marks<-c(80,85,92,74,68)
+mean(marks)
+median(marks)
+sd(marks)
+summary(marks)
